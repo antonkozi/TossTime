ed244a1e8234c0230cc027475972123a38ab2809
Cole Hennig <chennig@ucsc.edu>
2021-10-29 15:39:18 -0700

    xcode13

--- Toss_Time/Toss_Time/ViewController.swift
+++ Toss_Time/Toss_Time/ViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let manager = CLLocationManager()
+    
+    //@IBOutlet weak var mapView: GMSMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +22,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         manager.startUpdatingLocation()
         // Do any additional setup after loading the view.
         GMSServices.provideAPIKey("AIzaSyBXtU8FJoiP9w4tyLbUvZQ07oYJ7oc1pUQ")
-        
-        
-        
         print("licenseL \n\n\(GMSServices.openSourceLicenseInfo())")
+        //createMapView()
+        //self.mapView.mapStyle(withFilename: "theme", andType: "json")
     }
+    
+    /* func createMapView(){
+        let camera = GMSCameraPosition.camera(withLatitude: 36.9741, longitude: 122.0308, zoom: 15)
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+    } */
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else {
@@ -45,6 +52,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
 
-
 }
 
+
+/* extension GMSMapView {
+    func mapStyle (withFilename name: String, andType type: String) {
+        do {
+            if let styleURL = Bundle.main.url(forResource: name, withExtension: type) {
+                self.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+            } else {
+                NSLog("Unable to find darkMap")
+            }
+        }
+        catch {
+            NSLog("failded to load. \(error)")
+        }
+    }
+} */
