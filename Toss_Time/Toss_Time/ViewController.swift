dc03357fe21a3f21a402299c7cf28b92937db6cc
Anton <anton@Augies-MBP.attlocal.net>
2021-10-29 17:08:12 -0700

    added ability to create marker on tap

--- Toss_Time/Toss_Time/ViewController.swift
+++ Toss_Time/Toss_Time/ViewController.swift
@@ -9,38 +9,78 @@ import CoreLocation
 import GoogleMaps
 import UIKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
+    @IBOutlet weak var myMap: GMSMapView!
     
-    let manager = CLLocationManager()
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        manager.delegate = self
-        manager.requestWhenInUseAuthorization()
-        manager.startUpdatingLocation()
+        locationManager.delegate = self
+        myMap.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.requestLocation()
+            myMap.settings.zoomGestures=true
+        }
+        else{
+            locationManager.requestWhenInUseAuthorization()
+        }
         // Do any additional setup after loading the view.
-        GMSServices.provideAPIKey("AIzaSyBXtU8FJoiP9w4tyLbUvZQ07oYJ7oc1pUQ")
+        //GMSServices.provideAPIKey("AIzaSyBXtU8FJoiP9w4tyLbUvZQ07oYJ7oc1pUQ")
         
         
         
-        print("licenseL \n\n\(GMSServices.openSourceLicenseInfo())")
+        //print("licenseL \n\n\(GMSServices.openSourceLicenseInfo())")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        let lat = coordinate.latitude
+        let long = coordinate.longitude
+
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        marker.title = "hi"
+        marker.snippet = "hello"
+        marker.map = mapView
+
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first else {
-            return
-        }
-        let coordinate = location.coordinate
-        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
-        view.addSubview(mapView)
-        
+        myMap.camera = GMSCameraPosition(
+            target: CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0),
+            zoom: 8,
+            bearing: 0,
+            viewingAngle: 0)
+    
+       
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        
+        marker.position = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.latitude ?? 0.0)
+        marker.title = "hi"
+        marker.snippet = "hello"
+        marker.map = myMap
+
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus{
+        case .authorizedAlways:
+            return
+        case .authorizedWhenInUse:
+            return
+        case .denied:
+            return
+        case .restricted:
+            locationManager.requestWhenInUseAuthorization()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
     }
     
     
